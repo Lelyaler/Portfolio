d2ffae75852bd844b0623bcf1df50405e22e55f1
Lelyaler <you@example.com>
2024-01-10 20:22:49 +0300

    cv

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,140 +1,119 @@
+(function ($) {
+  var $window = $(window),
+    $body = $("body"),
+    $sidebar = $("#sidebar");
 
-(function($) {
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: [null, "480px"],
+  });
 
-	var	$window = $(window),
-		$body = $('body'),
-		$sidebar = $('#sidebar');
+  // Sidebar.
+  if ($sidebar.length > 0) {
+    var $sidebar_a = $sidebar.find("a");
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+    $sidebar_a
+      .addClass("scrolly")
+      .on("click", function () {
+        var $this = $(this);
 
-	// Sidebar.
-		if ($sidebar.length > 0) {
+        // External link? Bail.
+        if ($this.attr("href").charAt(0) != "#") return;
 
-			var $sidebar_a = $sidebar.find('a');
+        // Deactivate all links.
+        $sidebar_a.removeClass("active");
 
-			$sidebar_a
-				.addClass('scrolly')
-				.on('click', function() {
+        // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+        $this.addClass("active").addClass("active-locked");
+      })
+      .each(function () {
+        var $this = $(this),
+          id = $this.attr("href"),
+          $section = $(id);
 
-					var $this = $(this);
+        // No section for this link? Bail.
+        if ($section.length < 1) return;
 
-					// External link? Bail.
-						if ($this.attr('href').charAt(0) != '#')
-							return;
+        // Scrollex.
+        $section.scrollex({
+          mode: "middle",
+          top: "-20vh",
+          bottom: "-20vh",
+          initialize: function () {
+            // Deactivate section.
+            $section.addClass("inactive");
+          },
+          enter: function () {
+            // Activate section.
+            $section.removeClass("inactive");
 
-					// Deactivate all links.
-						$sidebar_a.removeClass('active');
+            // No locked links? Deactivate all links and activate this section's one.
+            if ($sidebar_a.filter(".active-locked").length == 0) {
+              $sidebar_a.removeClass("active");
+              $this.addClass("active");
+            }
 
-					// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-						$this
-							.addClass('active')
-							.addClass('active-locked');
+            // Otherwise, if this section's link is the one that's locked, unlock it.
+            else if ($this.hasClass("active-locked"))
+              $this.removeClass("active-locked");
+          },
+        });
+      });
+  }
 
-				})
-				.each(function() {
+  //inner__image slow
+  $(document).ready(function () {
+    $("#profile-img").addClass("show");
+  });
 
-					var	$this = $(this),
-						id = $this.attr('href'),
-						$section = $(id);
+  // Scrolly.
+  $(".scrolly").scrolly({
+    speed: 1000,
+    offset: function () {
+      // If <=large, >small, and sidebar is present, use its height as the offset.
+      if (
+        breakpoints.active("<=large") &&
+        !breakpoints.active("<=small") &&
+        $sidebar.length > 0
+      )
+        return $sidebar.height();
 
-					// No section for this link? Bail.
-						if ($section.length < 1)
-							return;
+      return 0;
+    },
+  });
 
-					// Scrollex.
-						$section.scrollex({
-							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
-							initialize: function() {
+  // portfolio.imgblock
+  $(".portfolio > section")
+    .scrollex({
+      mode: "middle",
+      top: "-10vh",
+      bottom: "-10vh",
+      initialize: function () {
+        // Deactivate section.
+        $(this).addClass("inactive");
+      },
+      enter: function () {
+        // Activate section.
+        $(this).removeClass("inactive");
+      },
+    })
+    .each(function () {
+      var $this = $(this),
+        $image = $this.find(".image"),
+        $img = $image.find("img"),
+        x;
 
-								// Deactivate section.
-									$section.addClass('inactive');
+      // Assign image.
+      $image.css("background-image", "url(" + $img.attr("src") + ")");
 
-							},
-							enter: function() {
+      // Set background position.
+      if ((x = $img.data("position"))) $image.css("background-position", x);
 
-								// Activate section.
-									$section.removeClass('inactive');
-
-								// No locked links? Deactivate all links and activate this section's one.
-									if ($sidebar_a.filter('.active-locked').length == 0) {
-
-										$sidebar_a.removeClass('active');
-										$this.addClass('active');
-
-									}
-
-								// Otherwise, if this section's link is the one that's locked, unlock it.
-									else if ($this.hasClass('active-locked'))
-										$this.removeClass('active-locked');
-
-							}
-						});
-
-				});
-
-		}
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000,
-			offset: function() {
-
-				// If <=large, >small, and sidebar is present, use its height as the offset.
-					if (breakpoints.active('<=large')
-					&&	!breakpoints.active('<=small')
-					&&	$sidebar.length > 0)
-						return $sidebar.height();
-
-				return 0;
-
-			}
-		});
-
-	// portfolio.imgblock
-		$('.portfolio > section')
-			.scrollex({
-				mode: 'middle',
-				top: '-10vh',
-				bottom: '-10vh',
-				initialize: function() {
-
-					// Deactivate section.
-						$(this).addClass('inactive');
-
-				},
-				enter: function() {
-
-					// Activate section.
-						$(this).removeClass('inactive');
-
-				}
-			})
-			.each(function() {
-
-				var	$this = $(this),
-					$image = $this.find('.image'),
-					$img = $image.find('img'),
-					x;
-
-				// Assign image.
-					$image.css('background-image', 'url(' + $img.attr('src') + ')');
-
-				// Set background position.
-					if (x = $img.data('position'))
-						$image.css('background-position', x);
-
-				// Hide <img>.
-					$img.hide();
-
-			});
-
+      // Hide <img>.
+      $img.hide();
+    });
 })(jQuery);
